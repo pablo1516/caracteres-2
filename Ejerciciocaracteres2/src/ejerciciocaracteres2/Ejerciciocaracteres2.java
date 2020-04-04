7b1bfbac43073605758b96d0fdb6f1100b7a6c88
pablo1516 <61156436+pablo1516@users.noreply.github.com>
2020-04-04 11:07:27 -0500

    Add files via upload

---
+++ Ejerciciocaracteres2/src/ejerciciocaracteres2/Ejerciciocaracteres2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocaracteres2;
+
+/**
+ *
+ * @author david
+ */
+public class Ejerciciocaracteres2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmMorse frmcarac = new FrmMorse();
+        frmcarac.setVisible(true);
+        frmcarac.setLocationRelativeTo(null);
+        frmcarac.setSize(400, 300);
+        frmcarac.setResizable(false);
+        frmcarac.setTitle("Caracteres 2");
+    }
+    
+}
